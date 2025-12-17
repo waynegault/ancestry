98005b4263ea05a44b9942f928eaf3c9e7d8d39b
Wayne Gault <39455578+waynegault@users.noreply.github.com>
2025-12-17 12:31:16 +0000

    Fix: Remove type:ignore in message_personalization.py for 100% quality

--- messaging/message_personalization.py
+++ messaging/message_personalization.py
@@ -43,7 +43,7 @@ except ImportError:
     def is_metrics_enabled() -> bool:
         return False
 
-    def metrics() -> Any:  # type: ignore[misc]
+    def metrics() -> None:
         return None
 
 
